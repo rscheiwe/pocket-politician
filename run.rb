651fb07234a5d552e7c6da33ef21a1474ff21d67
rscheiwe <rscheiwe@gmail.com>
2018-06-20 09:50:01 -0400

    reseeded database

--- run.rb
+++ run.rb
@@ -40,3 +40,24 @@ Pry.start
 #API Key 1c75e9b1efe4f5eb843c6397427019e9
 
 #==========================================
+#Cleaning database and resetting index
+#gem install database_cleaner
+#DatabaseCleaner.clean_with(:truncation, :only => ['yourtablename'])
+
+#==========================================
+# INSTRUCTIONS
+# 1. Welcome Message
+# 2. Instructions Message
+# 3. Name / State / Party input
+# 4. Hello ____
+# 5. puts Representative Card, w/ status_points & randomized CID
+# 6. Ask if they'd like an instructions reminder
+# 7. Or hit enter to begin
+# 8. Prints first bill
+# 8. Ask to support or vote against bill
+# 9. Politicians are printed for user to choose
+# 10. User selects politicians
+# 11. Success or Failure determined ( e.g, "Sorry, Rep X didn't accept your bribe")
+# 12. Print out Representative Card to show updated status_points
+# 13. Replay 8-12
+# 14. Break at X points for WIN, Y points for LOSS
